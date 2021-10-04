94a3ce7ef9b6c601c05b7b20a0772427f7cc8e0d
Jaime Rodríguez <jaimeromoreno@gmail.com>
2021-10-04 13:47:33 +0200

    get_patients endpoint test server

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + config.postgres_user +
 
 @app.route("/get_patients/", methods=['GET'])
 def index():
-    return requests.get(config.ccdr_url+"/api/v1/mobile/patient")
+    return requests.get(config.ccdr_url+"/api/v1/mobile/patient").json()
 
 
 if __name__ == '__main__':
