cbd7e7ccd87ce34bc6d5ac3c43ff37f8cdbe0195
Alberto del Río Ponce <alberdrp@gmail.com>
2022-05-03 16:00:21 +0200

    Sort notifications by priority

--- models/evaluation.py
+++ models/evaluation.py
@@ -3,7 +3,7 @@ import requests
 import numpy as np
 from datetime import datetime, timedelta
 
-from helper.utils import logger, game_notifications
+from helper.utils import game_notifications
 from helper import config
 
 
@@ -137,27 +137,56 @@ def game_evaluation(patient_reference, country_code):
 			game_summarization["stats"]["restarted"][str(day)] += summarization_day["session_interaction_results"][
 				"nclicks_game_restart"]
 
-	# Recommendation 1:
+	# Summarize mean stats
+	global_mean = []
+	score_mean = []
+	time_mean = []
+	interaction_mean = []
+	for game in range(6):
+		if game_summarization["metrics"]["type"]["global"][str(game + 1)]:
+			global_mean.append(np.mean(game_summarization["metrics"]["type"]["global"][str(game + 1)]))
+			score_mean.append(np.mean(game_summarization["metrics"]["type"]["score"][str(game + 1)]))
+			time_mean.append(np.mean(game_summarization["metrics"]["type"]["time"][str(game + 1)]))
+			interaction_mean.append(np.mean(game_summarization["metrics"]["type"]["interaction"][str(game + 1)]))
+		else:
+			global_mean.append(None)
+			score_mean.append(None)
+			time_mean.append(None)
+			interaction_mean.append(None)
+
+	# Recommendation 1.1
 	# Use frequently cognitive game app
 	if game_summarization["days_played"] < 3:
 		messages.append(game_notifications['R11'][country_code])
 
-	# Recommendation 2:
+	# Recommendation 1.2
+	# Play slowly
+	# If by average, any click value is lower than 5 seconds
+	avg_clicks_values_lower = [value for value in game_summarization["stats"]["time_between_clicks"] if value < 5]
+
+	# Check if len of appended values are more than 30% of the games completed and send notification if metrics are low
+	games_notification = []
+	if len(avg_clicks_values_lower) / len(game_summarization["stats"]["time_between_clicks"]) < 0.3:
+		for idx, game_mean in enumerate(global_mean):
+			if game_mean and game_mean < 0.5:
+				games_notification.append(idx + 1)
+	if games_notification:
+		games_notification = ",".join([str(item) for item in games_notification])
+		messages.append(game_notifications['R12'][country_code].format(str(games_notification)))
+
+	# Recommendation 1.3
+	# Complete the games
+	if len(game_summarization["games"]["global"]) < sum(game_summarization["stats"]["started"].values()) / 2:
+		messages.append(game_notifications['R13'][country_code])
+
+	# Recommendation 1.4
 	# Start a different game. Check the games played and compare with the whole list of 6 games
 	unique, counts = np.unique(game_summarization["games"]["global"], return_counts=True)
 	list_diff = np.setdiff1d(["1", "2", "3", "4", "5", "6"], list(unique))
 	if len(list_diff) > 0:
 		messages.append(game_notifications['R14'][country_code])
 
-	# Recommendation 3:
-	# Customize the app
-	for key in game_summarization["personalization"]:
-		uniques = np.unique(game_summarization["personalization"][key])
-
-		if not len(uniques) > 1:
-			messages.append(game_notifications['R31'][country_code])
-
-	# Recommendation 4:
+	# Recommendation 2.1
 	# Change game category
 	game_categories = []
 	for game in ["1", "5", "6"]:
@@ -173,7 +202,7 @@ def game_evaluation(patient_reference, country_code):
 		game_categories = ",".join([str(item) for item in game_categories])
 		messages.append(game_notifications['R21'][country_code].format(game_categories))
 
-	# Recommendation 5:
+	# Recommendation 2.2 / 2.3
 	# Change game level
 	game_levels_pos = []
 	game_levels_neg = []
@@ -193,24 +222,8 @@ def game_evaluation(patient_reference, country_code):
 		game_levels_neg = ",".join([str(item) for item in game_levels_neg])
 		messages.append(game_notifications['R23'][country_code].format(game_levels_neg))
 
-	# Recommendation 6:
+	# Recommendation 2.4
 	# Read carefully game information
-	global_mean = []
-	score_mean = []
-	time_mean = []
-	interaction_mean = []
-	for game in range(6):
-		if game_summarization["metrics"]["type"]["global"][str(game + 1)]:
-			global_mean.append(np.mean(game_summarization["metrics"]["type"]["global"][str(game+1)]))
-			score_mean.append(np.mean(game_summarization["metrics"]["type"]["score"][str(game+1)]))
-			time_mean.append(np.mean(game_summarization["metrics"]["type"]["time"][str(game+1)]))
-			interaction_mean.append(np.mean(game_summarization["metrics"]["type"]["interaction"][str(game+1)]))
-		else:
-			global_mean.append(None)
-			score_mean.append(None)
-			time_mean.append(None)
-			interaction_mean.append(None)
-
 	# Get games with values less than 0.5
 	for param in [global_mean, score_mean, time_mean, interaction_mean]:
 		values = [value for value in param if value if value < 0.5]
@@ -219,27 +232,15 @@ def game_evaluation(patient_reference, country_code):
 			messages.append(game_notifications['R24'][country_code])
 			break
 
-	# Recommendation 7:
-	# Complete the games
-	if len(game_summarization["games"]["global"]) < sum(game_summarization["stats"]["started"].values()) / 2:
-		messages.append(game_notifications['R13'][country_code])
+	# Recommendation 3.1
+	# Customize the app
+	for key in game_summarization["personalization"]:
+		uniques = np.unique(game_summarization["personalization"][key])
 
-	# Recommendation 8
-	# Play slowly
-	# If by average, any click value is lower than 5 seconds
-	avg_clicks_values_lower = [value for value in game_summarization["stats"]["time_between_clicks"] if value < 5]
+		if not len(uniques) > 1:
+			messages.append(game_notifications['R31'][country_code])
 
-	# Check if len of appended values are more than 30% of the games completed and send notification if metrics are low
-	games_notification = []
-	if len(avg_clicks_values_lower) / len(game_summarization["stats"]["time_between_clicks"]) < 0.3:
-		for idx, game_mean in enumerate(global_mean):
-			if game_mean and game_mean < 0.5:
-				games_notification.append(idx + 1)
-	if games_notification:
-		games_notification = ",".join([str(item) for item in games_notification])
-		messages.append(game_notifications['R12'][country_code].format(str(games_notification)))
-
-	# Recommendation 9:
+	# Recommendation 3.2 / 3.3 / 3.4
 	# Extract mean global metric and send notification based on results
 	if game_summarization["metrics"]["total"]["global"]:
 		mean_score_global = np.nanmean(np.array(game_summarization["metrics"]["total"]["global"], dtype=np.float64))
